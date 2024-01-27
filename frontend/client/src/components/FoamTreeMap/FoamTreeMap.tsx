a301d313a8d1a9e6521aed32cc86a20a53af28a2
art-in <artin@artin.space>
2024-01-27 18:32:50 +0000

    #2: tune FoamTree options

--- frontend/client/src/components/FoamTreeMap/FoamTreeMap.tsx
+++ frontend/client/src/components/FoamTreeMap/FoamTreeMap.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line
 // @ts-nocheck because FoamTree lib doesn't provide typings
-import {useCallback, useEffect, useRef, memo} from 'react';
 import FoamTree from '@carrotsearch/foamtree';
+import {useCallback, useEffect, useRef, memo} from 'react';
 import classes from './FoamTreeMap.module.css';
 import buildFoamTreeMapData from './buildFoamTreeMapData';
 
@@ -9,36 +9,61 @@ interface Props {
   profile: Profile;
 }
 
-// FoamTree API reference: https://get.carrotsearch.com/foamtree/latest/api/
 function FoamTreeMap(props: Props) {
   const elementRef = useRef<HTMLDivElement>(null);
-  const treeRef = useRef<Record<string, unknown> | null>(null);
+  const foamTreeRef = useRef<Record<string, unknown> | null>(null);
 
   useEffect(() => {
+    // FoamTree API reference:
+    // https://get.carrotsearch.com/foamtree/latest/api/
     if (!FoamTree.supported) {
       alert("FoamTree doesn't support this browser");
     }
 
-    treeRef.current = new FoamTree({
+    foamTreeRef.current = new FoamTree({
       element: elementRef.current,
+
       layout: 'squarified',
       stacking: 'flattened',
-      pixelRatio: window.devicePixelRatio || 1,
-      groupMinDiameter: 0,
+
+      // use a simple fade in/out animation
       rolloutDuration: 0,
       pullbackDuration: 0,
       fadeDuration: 0,
+
+      // use slightly lower than default border widths
+      groupBorderWidth: 2,
+      groupInsetWidth: 4,
+
+      // draw up to six levels of groups and labels
+      maxGroupLevelsDrawn: 6,
+      maxGroupLabelLevelsDrawn: 6,
+
+      groupExposureZoomMargin: -100,
+
+      // other options
+      pixelRatio: window.devicePixelRatio || 1,
+      descriptionGroupSize: 0.05,
+      groupMinDiameter: 0,
       groupExposureZoomMargin: 0.2,
-      zoomMouseWheelDuration: 50,
       openCloseDuration: 50,
+      maxLabelSizeForTitleBar: 0,
       groupFillType: 'plain',
       dataObject: buildFoamTreeMapData(props.profile),
+
+      // handlers
+
+      // zoom-in group on double click, instead of exposing it
+      onGroupDoubleClick: function (e) {
+        e.preventDefault();
+        this.zoom(e.secondary ? this.get('dataObject') : e.group);
+      },
     });
   }, []);
 
   const onWindowResize = useCallback(() => {
-    if (treeRef.current) {
-      treeRef.current.resize();
+    if (foamTreeRef.current) {
+      foamTreeRef.current.resize();
     }
   }, []);
 
