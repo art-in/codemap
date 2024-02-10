a8be377ff098ff65dc35ed3bff032a80df78846f
art-in <artin@artin.space>
2024-02-10 20:04:04 +0000

    #2: frontend/server: log http requests

--- frontend/server/src/server.ts
+++ frontend/server/src/server.ts
@@ -2,6 +2,7 @@ import fs from 'node:fs/promises';
 
 import crypto from 'crypto';
 import express from 'express';
+import morgan from 'morgan';
 
 import * as git from './utils/git';
 import * as loc from './utils/loc';
@@ -10,6 +11,8 @@ const LISTEN_PORT = 4000;
 const CLONNED_REPOS_DIR = './clonned-repos';
 
 const app = express();
+app.use(morgan('tiny'));
+
 const api = express.Router();
 
 api.get('/loc-profile', async (request, response) => {
@@ -32,8 +35,11 @@ api.get('/loc-profile', async (request, response) => {
 app.use('/api', api);
 
 (async () => {
+  // delete all failed clones of previous launch
   await fs.rm(CLONNED_REPOS_DIR, {recursive: true, force: true});
 
   app.listen(LISTEN_PORT);
+
   console.log('Listening at port ' + LISTEN_PORT);
+  console.log();
 })();
